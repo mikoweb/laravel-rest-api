e3e5a4b914a1972014c95d96750d37aaf73cb2ce
Rafał Mikołajun <root@rmweb.pl>
2024-09-12 20:40:10 +0200

    Added Swagger package

--- app/Core/UI/Controller/RestController.php
+++ app/Core/UI/Controller/RestController.php
@@ -2,8 +2,11 @@
 
 namespace App\Core\UI\Controller;
 
+use OpenApi\Attributes as OA;
 use WayOfDev\Serializer\Bridge\Laravel\Http\ResponseFactory;
 
+#[OA\Info(version: '0.0.1', title: 'Laravel REST API')]
+#[OA\Server(url: '/api')]
 abstract class RestController extends Controller
 {
     public function __construct(
